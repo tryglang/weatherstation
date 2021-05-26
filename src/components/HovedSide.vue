dd855ce4efadf2cbc2f866a65674cabfc4f8b103
Trygve <Trygve.langballe@hotmail.com>
2021-05-26 18:25:03 +0200

    fikset på kortene i hjemme menyen

--- src/components/HovedSide.vue
+++ src/components/HovedSide.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-      <h1>Værstasjon for Killingen</h1>
+      <h1 class="d-flex justify-center">Værstasjon for Killingen</h1>
+      <div>
+        <div class="d-flex justify-center flex-wrap">
       <v-card>
-        <h2>Lufttemperatur</h2>
+        <v-card-title>Lufttemperatur</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="d-flex justify-center">23°C</v-card-text>
       </v-card>
 
       <v-card>
-        <h2>Vanntemperatur</h2>
+        <v-card-title>Vanntemperatur</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="d-flex justify-center">16°C</v-card-text>
       </v-card>
-
+        </div>
+    <div class="d-flex justify-center flex-wrap">
       <v-card>
-        <h2>Vannstand</h2>
+        <v-card-title>Vannstand</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="d-flex justify-center">25cm</v-card-text>
       </v-card> 
 
+      <v-card>
+        <v-card-title>Vindfart</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text class="d-flex justify-center">5m/s</v-card-text>
+      </v-card>
+    </div>
+      </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
+<div>
   <HovedSide />
+    <p>
+      <router-link to="/">Home</router-link>
+    </p>
+    <p>
+      <router-link to="/about">About</router-link>
+    </p>
+</div>
 </template>
 
 <script>
