12fd8fbb8eef8f40c5e0db7d7d264447c0437b96
Trygve <Trygve.langballe@hotmail.com>
2021-05-26 13:59:41 +0200

    added some cards for measurements

--- src/components/HovedSide.vue
+++ src/components/HovedSide.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
       <h1>Værstasjon for Killingen</h1>
+      <v-card>
+        <h2>Lufttemperatur</h2>
+      </v-card>
+
+      <v-card>
+        <h2>Vanntemperatur</h2>
+      </v-card>
+
+      <v-card>
+        <h2>Vannstand</h2>
+      </v-card> 
+
   </div>
 </template>
 
