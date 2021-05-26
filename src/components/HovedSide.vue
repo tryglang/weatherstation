90c1b71ab2a17de467f0bc3442cc5809d2704f21
Trygve <Trygve.langballe@hotmail.com>
2021-05-26 20:18:10 +0200

    lagt til linker til de diverse undersidene

--- src/components/HovedSide.vue
+++ src/components/HovedSide.vue
@@ -3,30 +3,43 @@
       <h1 class="d-flex justify-center">Værstasjon for Killingen</h1>
       <div>
         <div class="d-flex justify-center flex-wrap">
-      <v-card>
-        <v-card-title>Lufttemperatur</v-card-title>
-        <v-divider></v-divider>
-        <v-card-text class="d-flex justify-center">23°C</v-card-text>
+      <v-card :width="200">
+        <router-link :to="{name: 'LuftTemp'}" style="text-decoration: none; color: inherit;">
+          <div>
+            <v-card-title class="justify-center">Lufttemperatur</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text class="d-flex justify-center">23°C</v-card-text>
+          </div>
+        </router-link>
       </v-card>
 
-      <v-card>
-        <v-card-title>Vanntemperatur</v-card-title>
+      <v-card :width="200">
+        <router-link :to="{name: 'VannTemp'}" style="text-decoration: none; color: inherit;">
+        <v-card-title class="justify-center">Vanntemperatur</v-card-title>
         <v-divider></v-divider>
         <v-card-text class="d-flex justify-center">16°C</v-card-text>
+        </router-link>
       </v-card>
         </div>
+        
     <div class="d-flex justify-center flex-wrap">
-      <v-card>
-        <v-card-title>Vannstand</v-card-title>
+
+      <v-card :width="200">
+        <router-link :to="{name: 'VannStand'}" style="text-decoration: none; color: inherit;">
+        <v-card-title class="justify-center">Vannstand</v-card-title>
         <v-divider></v-divider>
         <v-card-text class="d-flex justify-center">25cm</v-card-text>
+        </router-link>
       </v-card> 
 
-      <v-card>
-        <v-card-title>Vindfart</v-card-title>
+      <v-card :width="200">
+        <router-link :to="{name: 'Vind'}" style="text-decoration: none; color: inherit;">
+        <v-card-title class="justify-center">Vindfart</v-card-title>
         <v-divider></v-divider>
         <v-card-text class="d-flex justify-center">5m/s</v-card-text>
+        </router-link>
       </v-card>
+
     </div>
       </div>
   </div>
@@ -38,6 +51,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .linkcleaner{
+    text-decoration: none;
+    color: #121212;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import LuftTemp from '../views/LuftTemp.vue'
+import VannTemp from '../views/VannTemp.vue'
+import VannStand from '../views/VannStand.vue'
+import Vind from '../views/Vind.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +18,27 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/lufttemperatur',
+    name: 'LuftTemp',
+    component: LuftTemp
+  },
+  {
+    path: '/vanntemperatur',
+    name: 'VannTemp',
+    component: VannTemp
+  },
+  {
+    path: '/vannstand',
+    name: 'VannStand',
+    component: VannStand
+  },
+  {
+    path: '/vind',
+    name: 'Vind',
+    component: Vind
   }
 ]
 
